e2e0dbf9430e730065e13fea2cc62960d4a93da5
Eric de Quartel <edequartel@protonmail.com>
2022-11-24 07:07:37 +0100

    Initial Commit

---
+++ Bodypercussion/BodypercussionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodypercussionApp.swift
+//  Bodypercussion
+//
+//  Created by Eric de Quartel on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BodypercussionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
