7d28f4fc7e0945d7c488f554a0f421b8d1a306e6
Eric de Quartel <edequartel@protonmail.com>
2022-11-24 07:11:39 +0100

    Second commit

--- Bodypercussion/ContentView.swift
+++ Bodypercussion/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! test")
+            Text("Hello, world! test en meer")
         }
         .padding()
     }
